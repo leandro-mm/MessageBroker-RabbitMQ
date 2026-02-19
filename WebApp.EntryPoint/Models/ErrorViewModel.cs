5752b54a1f452eab6bad37bb3b335f6be570907d
Leandro M Machado <leandro-m-m@hotmail.com>
2026-02-19 16:16:06 -0300

    Add web project

---
+++ WebApp.EntryPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp.EntryPoint.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
