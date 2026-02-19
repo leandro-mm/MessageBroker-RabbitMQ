89bc59c171ff7e5e9f13ec00d71e4c5f398c5368
Leandro M Machado <leandro-m-m@hotmail.com>
2026-02-19 17:40:07 -0300

    adding publisher controller

---
+++ WebApp.EntryPoint/Controllers/PublisherController.cs
@@ -0,0 +1,34 @@
+using MessageBrokerRabbitMQ.Core;
+using MessageBrokerRabbitMQ.Core.Infrastructure.Queue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.EntryPoint.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PublisherController : ControllerBase
+{
+    private readonly PublisherManager publisherManager;
+
+    public PublisherController(PublisherManager publisherManager)
+    {
+        this.publisherManager = publisherManager;
+    }
+
+    [HttpPut]
+    public void AddPublisher([FromQuery] int size, [FromQuery] int messagesPerSecond)
+    {
+        this.publisherManager.AddPublisher(size, messagesPerSecond);
+    }
+    [HttpDelete("{id}")]
+    public void RemovePublisher(string id)
+    {
+        this.publisherManager.RemovePublisher(id);
+    }
+
+    [HttpGet()]
+    public IEnumerable<Publisher> GetPublishers()
+    {
+        return this.publisherManager.Publishers;
+    }
+}
